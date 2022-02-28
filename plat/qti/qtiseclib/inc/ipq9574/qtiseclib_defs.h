b66bd83d4392f352ed37b37acd934ea4e4e8123e
Ramdinesh J P <quic_jpr@quicinc.com>
2022-02-28 13:12:36 +0530

    [IPQ9574] Bringing up TMECOMM in ATF/arm-trusted-firmware
    - Configured TME interrupts in GIC
    
    Change-Id: Ic663673dd88b2cd7aee73eacd6057eb46e72e847
    Signed-off-by: Ramdinesh J P <quic_jpr@quicinc.com>

--- plat/qti/qtiseclib/inc/ipq9574/qtiseclib_defs.h
+++ plat/qti/qtiseclib/inc/ipq9574/qtiseclib_defs.h
@@ -30,25 +30,26 @@ typedef uintptr_t u_register_t;
 
 /* Interrupt number/ID defs. TODO: Chipset specific.
    Need to be feature gurded with Chipset Macro. */
-#define QTISECLIB_INT_ID_SEC_WDOG_BARK			(0xD5)
-#define QTISECLIB_INT_ID_NON_SEC_WDOG_BITE		(0x24)
-#define QTISECLIB_INT_ID_RESET_SGI			(0xf)
-#define QTISECLIB_INT_ID_CPU_WAKEUP_SGI			(0x8)
+#define QTISECLIB_INT_ID_SEC_WDOG_BARK                  (0xD5)
+#define QTISECLIB_INT_ID_NON_SEC_WDOG_BITE              (0x24)
+#define QTISECLIB_INT_ID_RESET_SGI                      (0xf)
+#define QTISECLIB_INT_ID_CPU_WAKEUP_SGI                 (0x8)
 
-#define QTISECLIB_INT_ID_VMIDMT_ERR_CLT_SEC		(0xE6)
-#define QTISECLIB_INT_ID_VMIDMT_ERR_CLT_NONSEC		(0xE7)
-#define QTISECLIB_INT_ID_VMIDMT_ERR_CFG_SEC		(0xE8)
-#define QTISECLIB_INT_ID_VMIDMT_ERR_CFG_NONSEC		(0xE9)
+#define QTISECLIB_INT_ID_VMIDMT_ERR_CLT_SEC             (0xE6)
+#define QTISECLIB_INT_ID_VMIDMT_ERR_CLT_NONSEC          (0xE7)
+#define QTISECLIB_INT_ID_VMIDMT_ERR_CFG_SEC             (0xE8)
+#define QTISECLIB_INT_ID_VMIDMT_ERR_CFG_NONSEC          (0xE9)
 
-#define QTISECLIB_INT_ID_XPU_SEC			(0xE3)
-#define QTISECLIB_INT_ID_XPU_NON_SEC			(0xE4)
+#define QTISECLIB_INT_ID_XPU_SEC                        (0xE3)
+#define QTISECLIB_INT_ID_XPU_NON_SEC                    (0xE4)
 
-#define QTISECLIB_INT_ID_MEM_NOC_ERROR			(0x71)
-#define QTISECLIB_INT_ID_SYSTEM_NOC_ERROR		(0xE1)
-#define QTISECLIB_INT_ID_PC_NOC_ERROR			(0xE2)
-#define QTISECLIB_INT_ID_AGGNOC_NOC_ERROR			(0x8E)
+#define QTISECLIB_INT_ID_MEM_NOC_ERROR                  (0x71)
+#define QTISECLIB_INT_ID_SYSTEM_NOC_ERROR               (0xE1)
+#define QTISECLIB_INT_ID_PC_NOC_ERROR                   (0xE2)
+#define QTISECLIB_INT_ID_AGGNOC_NOC_ERROR               (0x8E)
 #define QTISECLIB_INT_ID_AHB_TIMEOUT                    (0xE5)
-#define	QTISECLIB_INT_INVALID_INT_NUM			(0xFFFFFFFFU)
+#define QTISECLIB_INT_ID_TME_IPC                        (0x021E)
+#define QTISECLIB_INT_INVALID_INT_NUM                   (0xFFFFFFFFU)
 
 /* External CPU Dump Structure - 64 bit EL */
 typedef struct
