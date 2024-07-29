74eb898e2a5f27bd4e094f6783638c1c28f82969
Gowtham Pandi <quic_pgowth@quicinc.com>
2024-07-29 11:26:51 +0530

    ipq5424: Change in ATF start address
    1. ATF start address is moved from QSEE address range to smem range from marina ddr layout
    
    Change-Id: I6335c3e240de892e7dbeb37bfe171d8406df3edf
    Signed-off-by: Gowtham Pandi <quic_pgowth@quicinc.com>

--- plat/qti/ipq5424/inc/platform_def.h
+++ plat/qti/ipq5424/inc/platform_def.h
@@ -147,8 +147,8 @@
  * Put BL31 at DDR as per memory map. BL31_BASE is calculated using the
  * current BL31 debug size plus a little space for growth.
  */
-#define BL31_BASE						0x8A600000
-#define BL31_SIZE						0x80000
+#define BL31_BASE						0x8A832000
+#define BL31_SIZE						0x7D000
 #define QTI_TRUSTED_MAILBOX_SIZE				0x1000
 #define BL31_LIMIT						(BL31_BASE + BL31_SIZE - QTI_TRUSTED_MAILBOX_SIZE)
 
@@ -158,7 +158,7 @@
 /*
  * DIAG Start is placed 8K apart from __PIL_REGION_END__
  */
-#define QTI_DIAG_RG_START               0x8A669000
+#define QTI_DIAG_RG_START               0x8A89B000
 #define QTI_DIAG_RG_SIZE                0x3000
 #define QTI_DIAG_RG_LIMIT               QTI_DIAG_RG_START + QTI_DIAG_RG_SIZE
 
